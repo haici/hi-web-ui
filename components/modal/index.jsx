ce97bbe1e579b805cce6b32513b28ddd06dcec41
Yang <liuyang.programmer@gmail.com>
2018-01-11 14:46:18 +0800

    add modal style

--- components/modal/index.jsx
+++ components/modal/index.jsx
@@ -1,16 +1,43 @@
 import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
 import './index.less'
 
 class Modal extends Component {
+  constructor(props) {
+    super(props)
+    this.modalContainer = document.createElement('div')
+  }
+  componentDidMount() {
+    document.body.appendChild(this.modalContainer)
+  }
+  componentWillUnmount() {
+    document.body.removeChild(this.modalContainer)
+  }
   render() {
     const { hasShow = false } = this.props
     if (!hasShow) {
       return null
     }
-    return (
+    return ReactDOM.createPortal(
       <div className="hi-modal-root">
-        Modal
-      </div>
+        <div className="modal-box">
+          <div className="modal-title">
+            <span>标题</span>
+            <span className="modal-title-close">
+              <i className="iconfont icon-cha" />
+            </span>
+          </div>
+          <div className="modal-content">
+            内容
+          </div>
+          <div className="modal-bottom">
+            <button className="btn btn-cancel" >Cancel</button>
+            <button className="btn btn-submit">OK</button>
+          </div>
+        </div>
+        <div className="modal-back" />
+      </div>,
+      this.modalContainer,
     )
   }
 }
